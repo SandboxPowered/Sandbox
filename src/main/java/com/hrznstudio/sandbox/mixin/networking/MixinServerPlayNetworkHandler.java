de297246db13c75eae9588b7e86782c93d1b8df7
Coded <coded@hrznstudio.com>
2019-08-10 09:55:52 +0100

    Force serverside packets to be on the main thread

--- src/main/java/com/hrznstudio/sandbox/mixin/networking/MixinServerPlayNetworkHandler.java
+++ src/main/java/com/hrznstudio/sandbox/mixin/networking/MixinServerPlayNetworkHandler.java
@@ -3,11 +3,14 @@ package com.hrznstudio.sandbox.mixin.networking;
 import com.hrznstudio.sandbox.api.CustomPayloadPacket;
 import com.hrznstudio.sandbox.network.NetworkManager;
 import com.hrznstudio.sandbox.network.Packet;
+import net.minecraft.network.NetworkThreadUtils;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,8 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayNetworkHandler.class)
 public class MixinServerPlayNetworkHandler {
 
+    @Shadow
+    public ServerPlayerEntity player;
+
     @Inject(method = "onCustomPayload", at = @At("HEAD"), cancellable = true)
     public void onCustomPayload(CustomPayloadC2SPacket c2s, CallbackInfo info) {
+        NetworkThreadUtils.forceMainThread(c2s, (ServerPlayNetworkHandler) (Object) this, this.player.getServerWorld());
         CustomPayloadPacket customPayloadPacket = (CustomPayloadPacket) c2s;
         PacketByteBuf data = null;
         try {
