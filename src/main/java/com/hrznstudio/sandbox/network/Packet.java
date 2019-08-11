bb2460ca86e5cb7415bee5f4c351f37eefa9485a
Coded <coded@hrznstudio.com>
2019-08-11 16:08:56 +0100

    Remove IOExceptions

--- src/main/java/com/hrznstudio/sandbox/network/Packet.java
+++ src/main/java/com/hrznstudio/sandbox/network/Packet.java
@@ -2,12 +2,10 @@ package com.hrznstudio.sandbox.network;
 
 import net.minecraft.util.PacketByteBuf;
 
-import java.io.IOException;
-
 public interface Packet {
-    void read(PacketByteBuf var1) throws IOException;
+    void read(PacketByteBuf var1);
 
-    void write(PacketByteBuf var1) throws IOException;
+    void write(PacketByteBuf var1);
 
     void apply();
 }
