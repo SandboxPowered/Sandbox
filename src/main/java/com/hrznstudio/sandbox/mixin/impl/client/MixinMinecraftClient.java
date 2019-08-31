336edd83d350690045136f217f577cb35050e3f0
Coded <coded@hrznstudio.com>
2019-08-31 22:19:19 +0100

    :bug: Fix Client mismatch

--- src/main/java/com/hrznstudio/sandbox/mixin/impl/client/MixinMinecraftClient.java
+++ src/main/java/com/hrznstudio/sandbox/mixin/impl/client/MixinMinecraftClient.java
@@ -1,34 +1,43 @@
 package com.hrznstudio.sandbox.mixin.impl.client;
 
-import com.hrznstudio.sandbox.api.event.ScreenEvent;
-import com.hrznstudio.sandbox.event.EventDispatcher;
+import com.hrznstudio.sandbox.api.client.Client;
+import com.hrznstudio.sandbox.api.client.screen.IScreen;
+import com.hrznstudio.sandbox.api.entity.player.Player;
+import com.hrznstudio.sandbox.api.world.World;
 import com.hrznstudio.sandbox.util.WrappingUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.*;
 
 import javax.annotation.Nullable;
 
 @Mixin(MinecraftClient.class)
+@Implements(@Interface(iface = Client.class, prefix = "sbx$"))
+@Unique
 public class MixinMinecraftClient {
+
+    @Shadow
+    public ClientPlayerEntity player;
+
+    @Shadow
+    public ClientWorld world;
 
     @Shadow
     @Nullable
     public Screen currentScreen;
 
-    @ModifyVariable(method = "openScreen", at = @At("HEAD"), ordinal = 0)
-    public Screen openScreen(Screen screen) {
-        if (screen == null) {
-            ScreenEvent.Close close = EventDispatcher.publish(new ScreenEvent.Close(WrappingUtil.convert(currentScreen)));
-            if (close.isCancelled())
-                return currentScreen;
-            return null;
-        } else {
-            ScreenEvent.Open open = EventDispatcher.publish(new ScreenEvent.Open(WrappingUtil.convert(screen)));
-            return WrappingUtil.convert(open.getScreen());
-        }
+    public Player sbx$getPlayer() {
+        return (Player) player;
+    }
+
+    public World sbx$getWorld() {
+        return (World) world;
+    }
+
+    @Nullable
+    public IScreen sbx$getCurrentScreen() {
+        return WrappingUtil.convert(currentScreen);
     }
 }
