52699a9f8f7a9f3a0d4785e137a13a48cb5937cf
Coded <coded@hrznstudio.com>
2019-09-01 20:38:07 +0100

    :sparkles: Let lava-like fluids burn burnable blocks

--- src/main/java/com/hrznstudio/sandbox/mixin/impl/fluid/MixinBaseFluid.java
+++ src/main/java/com/hrznstudio/sandbox/mixin/impl/fluid/MixinBaseFluid.java
@@ -4,8 +4,11 @@ import com.hrznstudio.sandbox.SandboxProperties;
 import com.hrznstudio.sandbox.api.SandboxInternal;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FireBlock;
 import net.minecraft.fluid.BaseFluid;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
@@ -17,12 +20,15 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(BaseFluid.class)
-public abstract class MixinBaseFluid {
-    @Shadow protected abstract FluidState getUpdatedState(ViewableWorld viewableWorld_1, BlockPos blockPos_1, BlockState blockState_1);
+public abstract class MixinBaseFluid extends Fluid {
+    @Shadow
+    protected abstract FluidState getUpdatedState(ViewableWorld viewableWorld_1, BlockPos blockPos_1, BlockState blockState_1);
 
-    @Shadow protected abstract int getNextTickDelay(World world_1, BlockPos blockPos_1, FluidState fluidState_1, FluidState fluidState_2);
+    @Shadow
+    protected abstract int getNextTickDelay(World world_1, BlockPos blockPos_1, FluidState fluidState_1, FluidState fluidState_2);
 
-    @Shadow protected abstract void method_15725(IWorld iWorld_1, BlockPos blockPos_1, FluidState fluidState_1);
+    @Shadow
+    protected abstract void method_15725(IWorld iWorld_1, BlockPos blockPos_1, FluidState fluidState_1);
 
     /**
      * @author Coded
@@ -39,10 +45,10 @@ public abstract class MixinBaseFluid {
             }
         }
 
-        if(state!=null) {
+        if (state != null) {
             BlockState s = world_1.getBlockState(blockPos_1);
             int int_1 = this.getNextTickDelay(world_1, blockPos_1, fluidState_1, state);
-            if (s.isAir() || s.getBlock().getStateFactory().getProperty("fluidlogged") == null) {
+            if (s.isAir() || s.getBlock().getStateFactory().getProperty("fluidlogged") == null || (s.getMaterial().isBurnable() && this.matches(FluidTags.LAVA))) {
                 world_1.setBlockState(blockPos_1, state.isEmpty() ? Blocks.AIR.getDefaultState() : state.getBlockState(), 3);
                 if (!state.isEmpty()) {
                     world_1.getFluidTickScheduler().schedule(blockPos_1, state.getFluid(), int_1);
