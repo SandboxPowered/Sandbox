49e3562a33da28de6be927d474c5bf192dc44b47
Coded <coded@hrznstudio.com>
2019-09-03 14:45:34 +0100

    :fire: Remove unneeded mixins

--- src/main/java/com/hrznstudio/sandbox/mixin/impl/fluid/MixinBaseFluid.java
+++ src/main/java/com/hrznstudio/sandbox/mixin/impl/fluid/MixinBaseFluid.java
@@ -2,7 +2,6 @@ package com.hrznstudio.sandbox.mixin.impl.fluid;
 
 import com.hrznstudio.sandbox.api.SandboxInternal;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.fluid.BaseFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
@@ -11,7 +10,6 @@ import net.minecraft.world.IWorld;
 import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(BaseFluid.class)
@@ -30,35 +28,5 @@ public abstract class MixinBaseFluid extends Fluid implements SandboxInternal.Ba
     @Override
     public boolean sandboxinfinite() {
         return isInfinite();
-    }
-
-    /**
-     * @author Coded
-     */
-    @Overwrite
-    public void onScheduledTick(World world_1, BlockPos blockPos_1, FluidState fluidState_1) {
-        FluidState state = null;
-        if (!fluidState_1.isStill()) {
-            FluidState fluidState_2 = this.getUpdatedState(world_1, blockPos_1, world_1.getBlockState(blockPos_1));
-            if (fluidState_2.isEmpty()) {
-                state = fluidState_2;
-            } else if (!fluidState_2.equals(fluidState_1)) {
-                state = fluidState_2;
-            }
-        }
-
-        if (state != null) {
-            BlockState s = world_1.getBlockState(blockPos_1);
-            int int_1 = this.getNextTickDelay(world_1, blockPos_1, fluidState_1, state);
-            if (s.isAir() || s.getMaterial().isReplaceable()) {
-                world_1.setBlockState(blockPos_1, state.isEmpty() ? Blocks.AIR.getDefaultState() : state.getBlockState(), 3);
-                if (!state.isEmpty()) {
-                    world_1.getFluidTickScheduler().schedule(blockPos_1, state.getFluid(), int_1);
-                    world_1.updateNeighborsAlways(blockPos_1, s.getBlock());
-                }
-            }
-        }
-
-        this.method_15725(world_1, blockPos_1, fluidState_1);
     }
 }
