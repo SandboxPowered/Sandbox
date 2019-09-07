f498f34ddb54a14993a5b64ca3a6b65b64a3f11d
Coded <coded@hrznstudio.com>
2019-09-07 09:50:26 +0100

    :sparkles: implement drawRepeating

--- src/main/java/com/hrznstudio/sandbox/util/wrapper/RenderUtilImpl.java
+++ src/main/java/com/hrznstudio/sandbox/util/wrapper/RenderUtilImpl.java
@@ -24,22 +24,17 @@ public class RenderUtilImpl implements RenderUtil {
     }
 
     @Override
-    public void drawRepeating(int x, int y, int u, int v, int width, int height, float repeatWidth, float repeatHeight) {
+    public void drawRepeating(int x, int y, int u, int v, int width, int height, float repeatWidth, float repeatHeight, int texWidth, int texHeight) {
         GlStateManager.disableLighting();
         GlStateManager.disableFog();
         Tessellator tessellator_1 = Tessellator.getInstance();
         BufferBuilder bufferBuilder_1 = tessellator_1.getBufferBuilder();
         GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         bufferBuilder_1.begin(7, VertexFormats.POSITION_UV_COLOR);
-        bufferBuilder_1.vertex(x, y + height, 0.0D).texture(u/256f, (v + repeatHeight)/256f).color(255, 255, 255, 255).next();
-        bufferBuilder_1.vertex(x + width, y + height, 0.0D).texture((u + repeatWidth)/256f, (float) (v + repeatHeight)/256f).color(255, 255, 255, 255).next();
-        bufferBuilder_1.vertex(x + width, y, 0.0D).texture((u + repeatWidth)/256f, v/256f).color(255, 255, 255, 255).next();
-        bufferBuilder_1.vertex(x, y, 0.0D).texture(u/256f, v/256f).color(255, 255, 255, 255).next();
+        bufferBuilder_1.vertex(x, y + height, 0.0D).texture(u / (float) texWidth, (v + repeatHeight) / (float) texHeight).color(255, 255, 255, 255).next();
+        bufferBuilder_1.vertex(x + width, y + height, 0.0D).texture((u + repeatWidth) / (float) texWidth, (float) (v + repeatHeight) / (float) texHeight).color(255, 255, 255, 255).next();
+        bufferBuilder_1.vertex(x + width, y, 0.0D).texture((u + repeatWidth) / (float) texWidth, v / (float) texHeight).color(255, 255, 255, 255).next();
+        bufferBuilder_1.vertex(x, y, 0.0D).texture(u / (float) texWidth, v / (float) texHeight).color(255, 255, 255, 255).next();
         tessellator_1.draw();
-    }
-
-    @Override
-    public void drawRepeating(int x, int y, int u, int v, int width, int height, float repeatWidth, float repeatHeight, int texWidth, int texHeight) {
-        //TODO: implement
     }
 }
