c306719631fe007a7f8a116a4ffe013ffe8cdbd4
Coded <coded@hrznstudio.com>
2019-09-23 21:08:02 +0100

    :sparkles: Add loaded sandbox addons to crash report

--- src/main/java/com/hrznstudio/sandbox/mixin/fabric/crash/MixinCrashReport.java
+++ src/main/java/com/hrznstudio/sandbox/mixin/fabric/crash/MixinCrashReport.java
@@ -1,5 +1,7 @@
 package com.hrznstudio.sandbox.mixin.fabric.crash;
 
+import com.hrznstudio.sandbox.client.SandboxClient;
+import com.hrznstudio.sandbox.server.SandboxServer;
 import net.minecraft.util.crash.CrashReport;
 import net.minecraft.util.crash.CrashReportSection;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,7 +20,32 @@ public abstract class MixinCrashReport {
     private void fillSystemDetails(CallbackInfo info) {
         getSystemDetailsSection().add("Sandbox Addons", () -> {
             StringBuilder builder = new StringBuilder();
-            builder.append("Sandbox@1.0.0");
+            if (SandboxServer.INSTANCE != null) {
+                builder.append("Server: \n");
+                SandboxServer.INSTANCE.loader.getAddons().forEach(spec -> {
+                    builder.append("- ").append(spec.getModid());
+
+                    if (!spec.getTitle().equals(spec.getModid())) {
+                        builder.append(" \"").append(spec.getTitle()).append("\"");
+                    }
+
+                    builder.append(" @ ").append(spec.getVersion().toString()).append("\n");
+                });
+            }
+
+            if (SandboxClient.INSTANCE != null) {
+                builder.append("Client: \n");
+                SandboxClient.INSTANCE.loader.getAddons().forEach(spec -> {
+                    builder.append("- ").append(spec.getModid());
+
+                    if (!spec.getTitle().equals(spec.getModid())) {
+                        builder.append(" \"").append(spec.getTitle()).append("\"");
+                    }
+
+                    builder.append(" @ ").append(spec.getVersion().toString()).append("\n");
+                });
+            }
+
             return builder.toString();
         });
     }
