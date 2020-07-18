80e2474e6f26266d6fccd810d44c6f03b13de6cb
Coded <coded@voxel.games>
2020-07-18 10:04:11 +0100

    Move internal loading to impl

--- src/main/java/org/sandboxpowered/sandbox/fabric/SandboxFabric.java
+++ src/main/java/org/sandboxpowered/sandbox/fabric/SandboxFabric.java
@@ -1,5 +1,8 @@
 package org.sandboxpowered.sandbox.fabric;
 
+import com.github.zafarkhaja.semver.Parser;
+import com.github.zafarkhaja.semver.expr.Expression;
+import com.github.zafarkhaja.semver.expr.ExpressionParser;
 import com.google.common.collect.ImmutableMap;
 import net.fabricmc.loader.api.FabricLoader;
 import org.sandboxpowered.api.SandboxAPI;
@@ -8,17 +11,17 @@ import org.sandboxpowered.api.addon.AddonInfo;
 import org.sandboxpowered.api.content.Content;
 import org.sandboxpowered.api.registry.Registrar;
 import org.sandboxpowered.api.registry.Registry;
+import org.sandboxpowered.api.resources.ResourceManager;
 import org.sandboxpowered.api.util.Identity;
 import org.sandboxpowered.api.util.Log;
 import org.sandboxpowered.api.util.Side;
 import org.sandboxpowered.internal.Sandbox;
 import org.sandboxpowered.sandbox.fabric.util.AddonLog;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class SandboxFabric implements Sandbox {
+    private static final Parser<Expression> PARSER = ExpressionParser.newInstance();
     private static final Identity PLATFORM = Identity.of("sandboxpowered", "fabric");
 
     private final Map<String, AddonInfo> loadedAddons = new LinkedHashMap<>();
@@ -29,6 +32,65 @@ public class SandboxFabric implements Sandbox {
     public void loadAddon(AddonInfo info, Addon addon) {
         loadedAddons.put(info.getId(), info);
         addonMap.put(info, addon);
+    }
+
+    private List<AddonInfo> getLoadOrder() {
+        Set<AddonInfo> visited = new HashSet<>();
+        List<AddonInfo> loadOrder = new ArrayList<>();
+        for (AddonInfo info : getAllAddons().keySet()) {
+            handleDependencies(visited, loadOrder, info);
+        }
+        return loadOrder;
+    }
+
+    public void initAll() {
+        getLoadOrder().forEach(info -> {
+            if (!info.getPlatformSupport(getPlatform()).canRun()) {
+                throw new IllegalStateException(String.format("Addon %s cannot run on platform %s!", info.getId(), getPlatform().toString()));
+            }
+            Addon addon = getAllAddons().get(info);
+            SandboxAPI api = getAPIFor(info);
+            try {
+                addon.init(api);
+            } catch (Exception e) {
+                throw new RuntimeException(String.format("Initialization for addon %s failed: %s", info.getId(), e.getMessage()), e);
+            }
+        });
+    }
+
+    public void registerAll() {
+        getLoadOrder().forEach(info -> {
+            if (!info.getPlatformSupport(getPlatform()).canRun()) {
+                throw new IllegalStateException(String.format("Addon %s cannot run on platform %s!", info.getId(), getPlatform().toString()));
+            }
+            Addon addon = getAllAddons().get(info);
+            Registrar registrar = getRegistrarFor(info);
+            try {
+                addon.register(registrar);
+            } catch (Exception e) {
+                throw new RuntimeException(String.format("Registration for addon %s failed: %s", info.getId(), e.getMessage()), e);
+            }
+        });
+        ResourceManager.register();
+    }
+
+    //TODO: does this properly prevent circular dependencies while satisfying everything?
+    private void handleDependencies(Set<AddonInfo> visited, List<AddonInfo> order, AddonInfo info) {
+        if (visited.contains(info)) return;
+        visited.add(info);
+        Map<String, String> dependencies = info.getDependencies();
+        for (String dep : dependencies.keySet()) {
+            Optional<AddonInfo> optionalDep = getAddon(dep);
+            if (!optionalDep.isPresent())
+                throw new IllegalStateException(String.format("Addon %s depends on other addon %s that isn't loaded!", info.getId(), dep));
+            AddonInfo dependency = optionalDep.get();
+            String versionString = dependencies.get(dep);
+            Expression version = PARSER.parse(versionString);
+            if (!version.interpret(dependency.getVersion()))
+                throw new IllegalStateException(String.format("Addon %s depends on %s version %s but found version %s instead!", info.getId(), dep, versionString, dependency.getVersion().toString()));
+            handleDependencies(visited, order, info);
+        }
+        order.add(info);
     }
 
     @Override
