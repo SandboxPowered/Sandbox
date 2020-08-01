424794fa17492cdef977f5b6843fdffbc55a48de
Coded <coded@voxel.games>
2020-08-01 15:21:15 +0100

    Add side shapes into calculation in test mod

--- TestAddon/src/main/java/org/sandboxpowered/example/PipeBlock.java
+++ TestAddon/src/main/java/org/sandboxpowered/example/PipeBlock.java
@@ -3,6 +3,7 @@ package org.sandboxpowered.example;
 import org.sandboxpowered.api.block.BaseBlock;
 import org.sandboxpowered.api.block.Block;
 import org.sandboxpowered.api.client.GraphicsMode;
+import org.sandboxpowered.api.component.Components;
 import org.sandboxpowered.api.entity.player.Hand;
 import org.sandboxpowered.api.entity.player.PlayerEntity;
 import org.sandboxpowered.api.item.ItemStack;
@@ -18,25 +19,46 @@ import org.sandboxpowered.api.world.World;
 import org.sandboxpowered.api.world.WorldReader;
 
 import java.util.EnumMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class PipeBlock extends BaseBlock {
     public static final Shape CORE = Shape.cuboid(4, 4, 4, 12, 12, 12);
-    public static final Map<Direction, Property<Boolean>> DIRECTION_PROPERTY_MAP = new EnumMap<>(Direction.class);
+    public static final Map<Direction, Property<Boolean>> CONNECTION_PROPERTIES = new EnumMap<>(Direction.class);
     public static final Property<Boolean> UP = Properties.UP;
     public static final Property<Boolean> DOWN = Properties.DOWN;
     public static final Property<Boolean> NORTH = Properties.NORTH;
     public static final Property<Boolean> EAST = Properties.EAST;
     public static final Property<Boolean> SOUTH = Properties.SOUTH;
     public static final Property<Boolean> WEST = Properties.WEST;
+    private static final Map<Direction, Shape> SIDE_SHAPES = new EnumMap<>(Direction.class);
+
+    private final Map<BlockState, Shape> SHAPES = new IdentityHashMap<>();
 
     static {
-        DIRECTION_PROPERTY_MAP.put(Direction.UP, UP);
-        DIRECTION_PROPERTY_MAP.put(Direction.DOWN, DOWN);
-        DIRECTION_PROPERTY_MAP.put(Direction.NORTH, NORTH);
-        DIRECTION_PROPERTY_MAP.put(Direction.EAST, EAST);
-        DIRECTION_PROPERTY_MAP.put(Direction.SOUTH, SOUTH);
-        DIRECTION_PROPERTY_MAP.put(Direction.WEST, WEST);
+        CONNECTION_PROPERTIES.put(Direction.UP, UP);
+        CONNECTION_PROPERTIES.put(Direction.DOWN, DOWN);
+        CONNECTION_PROPERTIES.put(Direction.NORTH, NORTH);
+        CONNECTION_PROPERTIES.put(Direction.EAST, EAST);
+        CONNECTION_PROPERTIES.put(Direction.SOUTH, SOUTH);
+        CONNECTION_PROPERTIES.put(Direction.WEST, WEST);
+
+        SIDE_SHAPES.put(Direction.UP, Shape.cuboid(4, 12, 4, 12, 16, 12));
+        SIDE_SHAPES.put(Direction.DOWN, Shape.cuboid(4, 0, 4, 12, 4, 12));
+        SIDE_SHAPES.put(Direction.NORTH, Shape.cuboid(4, 4, 0, 12, 12, 4));
+        SIDE_SHAPES.put(Direction.EAST, Shape.cuboid(12, 4, 4, 16, 12, 12));
+        SIDE_SHAPES.put(Direction.SOUTH, Shape.cuboid(4, 4, 12, 12, 12, 16));
+        SIDE_SHAPES.put(Direction.WEST, Shape.cuboid(0, 4, 4, 4, 12, 12));
+    }
+
+    private static Shape computeShape(BlockState state) {
+        Shape shape = CORE;
+        for (Direction direction : Direction.ALL) {
+            if (state.get(CONNECTION_PROPERTIES.get(direction))) {
+                shape = shape.combine(SIDE_SHAPES.get(direction)).simplify();
+            }
+        }
+        return shape;
     }
 
     public PipeBlock(Settings settings) {
@@ -51,20 +73,21 @@ public class PipeBlock extends BaseBlock {
 
     @Override
     public BlockState getStateForPlacement(WorldReader reader, Position pos, PlayerEntity player, Hand hand, ItemStack stack, Direction side, Vec3d hitPos) {
-
         BlockState state = getBaseState();
-
         for (Direction direction : Direction.ALL) {
-            state = state.with(DIRECTION_PROPERTY_MAP.get(direction), reader.getBlockState(pos.offset(direction)).is(this));
+            Position offsetPos = pos.offset(direction);
+            state = state.with(CONNECTION_PROPERTIES.get(direction), canConnectTo(reader, offsetPos, reader.getBlockState(offsetPos)));
         }
-
         return state;
+    }
+
+    public boolean canConnectTo(WorldReader reader, Position pos, BlockState state) {
+        return state.getBlock() instanceof PipeBlock || state.getComponent(reader, pos, Components.INVENTORY_COMPONENT).isPresent();
     }
 
     @Override
     public BlockState updateOnNeighborChanged(BlockState state, Direction direction, BlockState otherState, World world, Position position, Position otherPosition) {
-        state = state.with(DIRECTION_PROPERTY_MAP.get(direction), world.getBlockState(otherPosition).is(this));
-        return state;
+        return state.with(CONNECTION_PROPERTIES.get(direction), canConnectTo(world, otherPosition, otherState));
     }
 
     @Override
@@ -74,6 +97,6 @@ public class PipeBlock extends BaseBlock {
 
     @Override
     public Shape getShape(WorldReader reader, Position position, BlockState state) {
-        return CORE;
+        return SHAPES.computeIfAbsent(state, PipeBlock::computeShape);
     }
 }
