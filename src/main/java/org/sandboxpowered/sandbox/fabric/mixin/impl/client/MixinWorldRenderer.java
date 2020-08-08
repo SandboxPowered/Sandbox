3a0ff224e2f03511f2f5cc9e48e2f16400f5833a
Coded <coded@voxel.games>
2020-08-08 21:00:30 +0100

    Cleanup world border cycling

--- src/main/java/org/sandboxpowered/sandbox/fabric/mixin/impl/client/MixinWorldRenderer.java
+++ src/main/java/org/sandboxpowered/sandbox/fabric/mixin/impl/client/MixinWorldRenderer.java
@@ -10,23 +10,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
-    private static final Identifier DOTS = new Identifier("sandbox", "textures/misc/dot.png");
-    private static final Identifier LINES = new Identifier("sandbox", "textures/misc/lines.png");
-    private static final Identifier GRID = new Identifier("sandbox", "textures/misc/grid.png");
-
     @Redirect(method = "renderWorldBorder", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/TextureManager;bindTexture(Lnet/minecraft/util/Identifier;)V"))
     public void bindTexture(TextureManager manager, Identifier identifier) {
-        switch (SandboxOptions.border) {
-            case LINES:
-                identifier = LINES;
-                break;
-            case GRID:
-                identifier = GRID;
-                break;
-            case DOTS:
-                identifier = DOTS;
-                break;
-        }
+        if (SandboxOptions.border.getTexture() != null)
+            identifier = SandboxOptions.border.getTexture();
         manager.bindTexture(identifier);
     }
 }
