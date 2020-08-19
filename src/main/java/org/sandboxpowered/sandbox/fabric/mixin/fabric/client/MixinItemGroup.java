742889a770c88e78ce8adb1d159807461d1a7967
Coded <coded@voxel.games>
2020-08-19 09:25:12 +0100

    Add creative & op only items to the creative menu (when applicable)

---
+++ src/main/java/org/sandboxpowered/sandbox/fabric/mixin/fabric/client/MixinItemGroup.java
@@ -0,0 +1,44 @@
+package org.sandboxpowered.sandbox.fabric.mixin.fabric.client;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemGroup.class)
+public class MixinItemGroup {
+
+    @Shadow
+    @Final
+    private int index;
+
+    @Inject(method = "appendStacks", at = @At("RETURN"))
+    public void appendStacks(DefaultedList<ItemStack> stacks, CallbackInfo info) {
+        boolean lv2 = MinecraftClient.getInstance().player.hasPermissionLevel(2);
+        if (lv2 && this.index == ItemGroup.TRANSPORTATION.getIndex()) {
+            stacks.add(new ItemStack(Items.COMMAND_BLOCK_MINECART));
+        }
+        if (this.index == ItemGroup.BUILDING_BLOCKS.getIndex()) {
+            stacks.add(new ItemStack(Items.BARRIER));
+        }
+        if (this.index == ItemGroup.DECORATIONS.getIndex()) {
+            stacks.add(new ItemStack(Items.DRAGON_EGG));
+        }
+        if (lv2 && this.index == ItemGroup.REDSTONE.getIndex()) {
+            stacks.add(new ItemStack(Items.COMMAND_BLOCK));
+        }
+        if (lv2 && this.index == ItemGroup.MISC.getIndex()) {
+            stacks.add(new ItemStack(Items.STRUCTURE_BLOCK));
+            stacks.add(new ItemStack(Items.STRUCTURE_VOID));
+            stacks.add(new ItemStack(Items.DEBUG_STICK));
+        }
+    }
+}
