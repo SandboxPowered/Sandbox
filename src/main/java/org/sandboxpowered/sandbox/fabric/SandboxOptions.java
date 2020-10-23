535c226eeae84c77572aec50cd0cb9ecb4f7f906
Coded <coded@voxel.games>
2020-10-23 17:58:41 +0100

    Fix world border option having incorrect label

--- src/main/java/org/sandboxpowered/sandbox/fabric/SandboxOptions.java
+++ src/main/java/org/sandboxpowered/sandbox/fabric/SandboxOptions.java
@@ -10,7 +10,8 @@ public class SandboxOptions {
 
     public static final CyclingOption WORLD_BORDER = new CyclingOption("options.sandbox.worldborder",
             (gameOptions, integer) -> SandboxConfig.updateBorderType(SandboxConfig.worldBorder.getEnum(SandboxConfig.WorldBorder.class).next()),
-            (options, cyclingOption) -> new TranslatableText(SandboxConfig.worldBorder.getEnum(SandboxConfig.WorldBorder.class).getTranslation()));
+            (options, cyclingOption) -> cyclingOption.getGenericLabel(new TranslatableText(SandboxConfig.worldBorder.getEnum(SandboxConfig.WorldBorder.class).getTranslation()))
+    );
 
     public static final BooleanOption CULL_PARTICLES = new BooleanOption("options.sandbox.cullparticles",
             options -> SandboxConfig.cullParticles.get(),
